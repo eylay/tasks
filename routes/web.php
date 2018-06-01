4926f2f54baecaced9ffa7a25ee2191598acbbfb
eylay <ali.seyfi68@gmail.com>
2018-06-01 19:15:22 +0430

    r21

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('tasks/create','TaskController@create');
-Route::get('tasks/{task}','TaskController@show')->where('task', '[0-9]+');
-Route::get('tasks/{task}/edit','TaskController@edit')->where('task', '[0-9]+');
-Route::get('tasks','TaskController@index');
-Route::post('tasks','TaskController@store');
-Route::put('tasks/{task}','TaskController@update')->where('task', '[0-9]+');
-Route::delete('tasks/{task}','TaskController@destroy')->where('task', '[0-9]+');
+Route::resource('tasks','TaskController');
